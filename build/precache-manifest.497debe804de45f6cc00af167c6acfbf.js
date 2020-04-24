0784b2c5e5559bbdef355ea1840441d88b9c6487
Дмитрий Мирошниченко <d.mir.posts@gmail.com>
2020-04-24 23:41:32 +0300

    Initial build subtree commit

---
+++ build/precache-manifest.497debe804de45f6cc00af167c6acfbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7304fe00fdcf862054fbc963c4da1b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fe365bba8c9e1c766df",
+    "url": "/static/css/main.737af8a2.chunk.css"
+  },
+  {
+    "revision": "21f3adbcd9f786de2f29",
+    "url": "/static/js/2.a2f0a224.chunk.js"
+  },
+  {
+    "revision": "3185f0705e32586709864fdc87a95a87",
+    "url": "/static/js/2.a2f0a224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe365bba8c9e1c766df",
+    "url": "/static/js/main.9e43fcb0.chunk.js"
+  },
+  {
+    "revision": "0ca69d042f856816c9f8",
+    "url": "/static/js/runtime-main.85ac5dc1.js"
+  }
+]);
