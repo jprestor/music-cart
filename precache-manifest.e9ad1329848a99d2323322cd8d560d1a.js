b1e8b831ed663f078c2e8fdc18d10f0b1a99c914
Дмитрий Мирошниченко <d.mir.posts@gmail.com>
2020-04-25 11:11:10 +0300

    Updates

---
+++ precache-manifest.e9ad1329848a99d2323322cd8d560d1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1cabc9d73800ff2a70796767d8defca",
+    "url": "/music-cart/index.html"
+  },
+  {
+    "revision": "40b037d3ae5bfcd6cdaf",
+    "url": "/music-cart/static/css/main.129260d3.chunk.css"
+  },
+  {
+    "revision": "21f3adbcd9f786de2f29",
+    "url": "/music-cart/static/js/2.a2f0a224.chunk.js"
+  },
+  {
+    "revision": "3185f0705e32586709864fdc87a95a87",
+    "url": "/music-cart/static/js/2.a2f0a224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40b037d3ae5bfcd6cdaf",
+    "url": "/music-cart/static/js/main.7d52ef6f.chunk.js"
+  },
+  {
+    "revision": "f9e67c0bb76639b3778a",
+    "url": "/music-cart/static/js/runtime-main.bec336fe.js"
+  }
+]);
