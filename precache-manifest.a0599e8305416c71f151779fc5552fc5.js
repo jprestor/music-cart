93c7a8b2adfbf1750591d28728d81a7998744287
Дмитрий Мирошниченко <d.mir.posts@gmail.com>
2020-04-25 12:01:16 +0300

    Updates

--- precache-manifest.a0599e8305416c71f151779fc5552fc5.js
+++ precache-manifest.a0599e8305416c71f151779fc5552fc5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "28c4647e887bb549dd47279abec084b3",
+    "revision": "d729edeb6d87967e95a60e46d0b84a3b",
     "url": "/music-cart/index.html"
   },
   {
-    "revision": "8ba7322d4b2a8bcf8f6c",
+    "revision": "4ef4896fc115d4a7b504",
     "url": "/music-cart/static/css/main.3059333e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/music-cart/static/js/2.dbbed4c8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8ba7322d4b2a8bcf8f6c",
-    "url": "/music-cart/static/js/main.74d7e149.chunk.js"
+    "revision": "4ef4896fc115d4a7b504",
+    "url": "/music-cart/static/js/main.0b5a1807.chunk.js"
   },
   {
     "revision": "f9e67c0bb76639b3778a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/music-cart/precache-manifest.1e346ac2bc08b79308c7d4e304349d7c.js"
+  "/music-cart/precache-manifest.a0599e8305416c71f151779fc5552fc5.js"
 );
 
 self.addEventListener('message', (event) => {
